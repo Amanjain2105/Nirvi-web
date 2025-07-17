c5f180c51cb127743fc6069115dc19419670f619
AmanJain <amanjainamson@gmail.com>
2025-07-17 19:48:19 +0530

    Initial commit for e-commerce project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
+import CategoryList from "@/components/CategoryList";
+import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import Slider from "@/components/Slider";
+import { Suspense } from "react";
+
 const HomePage = () => {
   return (
-    <div className=''>HomePage</div>
-  )
-}
+    <div>
+      <Slider />
 
-export default HomePage
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">Featured Products</h1>
+        <Suspense fallback={<Skeleton />}>
+          {/* ProductList will fetch server-side */}
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
+      </div>
+
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <Suspense fallback={<Skeleton />}>
+          <CategoryList />
+        </Suspense>
+      </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
